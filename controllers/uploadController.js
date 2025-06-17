cc9f9e7ce331f7f427675a2b94f1388516e05644
topickering <topickering@gmail.com>
2025-06-17 17:24:26 +0100

    refactor parse-csv function, extract from controller, tests

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -2,41 +2,29 @@ import fs from 'fs';
 import csv from 'csv-parser';
 import { addToUploadDB, getProgressById, getUploadDataById } from '../lib/uploadDB.js';
 import logger from '../lib/logger.js';
+import parseCSV from '../lib/parse-csv.js';
 
 async function postUpload(req, res) {    
-    const  { file } = req
+    const  { file } = req;
     
     if(!file) {
         return res.status(400).send('No file uploaded');
     }
    
-    const results = [];
+    logger.info(`Commencing parsing of file: ${file.filename}`);
+    try {
+        const results = await parseCSV(file);
+        const uploadId = addToUploadDB(results);
 
-    logger.info('Commencing parsing of file ' + file.filename);
-    
-    const stream = fs
-        .createReadStream(file.path)
-        .pipe(csv())
-        .on('data', (data) => {
-            results.push(data)
-        })
-        .on('end', () => {
-
-            logger.info('Completed parsing of file ' + file.filename);
-            const uploadId = addToUploadDB(results);
-
-            res.setHeader('Content-Type', 'application/json')
-                .status(200)
-                .send(JSON.stringify({
-                    uploadId,
-                    message: 'File uploaded successfully. Processing started.'
-                }))
-        });
-
-    stream.on('error', (err) => {
-        console.error('Stream error:', err)
-        res.status(500).send('Error reading file stream.')
-    })    
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200)
+            .send({
+                uploadId,
+                message: 'File uploaded successfully. Processing started.'
+            });
+    } catch(error) {
+        res.status(500).send(`Error processing file upload: ${error.msg}`);
+    }  ;
 }
 
 function getUpload(req, res) {
@@ -44,11 +32,11 @@ function getUpload(req, res) {
 
     if(!id) return res.status(400).send('Must provide an id');
 
-    const progress = getProgressById(id)
+    const progress = getProgressById(id);
 
     if(progress != 100) return res.status(400).send("Upload still being processed. Visit '/status/{uploadId}");
 
-    const { results, processedRecords, failedRecords, details } = getUploadDataById(id)
+    const { results, processedRecords, failedRecords, details } = getUploadDataById(id);
 
     res.setHeader('Content-Type', 'application/json');
     res.status(200).send({
--- controllers/uploadController.test.js
+++ controllers/uploadController.test.js
@@ -1,5 +1,10 @@
 import { afterEach, describe, it, expect, vi, beforeEach } from 'vitest';
 import { getUpload, postUpload } from './uploadController.js';
+import { addToUploadDB, getProgressById, getUploadDataById } from '../lib/uploadDB.js';
+import parseCSV from '../lib/parse-csv.js';
+
+vi.mock('../lib/uploadDB.js')
+vi.mock('../lib/parse-csv.js')
 
 const mockRes = () => ({
     send: vi.fn(),
@@ -30,11 +35,120 @@ describe('upload controller', () => {
             await postUpload(req, res);
 
             expect(res.status).toHaveBeenCalledWith(400);
-            expect(res.send).toHaveBeenCalled('No file uploaded')
+            expect(res.send).toHaveBeenCalledWith('No file uploaded')
         });
 
-        
+        it('sends a 200 and uploads results to DB if file parsed successfully', async () => {
+            const file = {
+                    filename: 'testFile',
+                }
+            
+            let req = {
+                file
+            };
+            let res = mockRes();
 
+            res.status.mockImplementation(() => res);
+            res.setHeader.mockImplementation(() => res);
+            parseCSV.mockResolvedValue([{test: 'record'}]);
+            addToUploadDB.mockImplementation(() => 'testId')
 
+            await postUpload(req, res);
+
+            expect(parseCSV).toHaveBeenCalledWith(file);
+            expect(addToUploadDB).toHaveBeenCalledWith([{test: 'record'}])
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith({
+                uploadId: 'testId',
+                message: 'File uploaded successfully. Processing started.'
+            })
+        });
+
+        it('sends a 500 if file parsed is unsuccessful', async () => {
+            const file = {
+                    filename: 'testFile',
+                }
+            
+            let req = {
+                file
+            };
+            let res = mockRes();
+
+            res.status.mockImplementation(() => res);
+            res.setHeader.mockImplementation(() => res);
+            parseCSV.mockRejectedValue({msg: 'error'});
+
+            await postUpload(req, res);
+
+            expect(parseCSV).toHaveBeenCalledWith(file);
+            expect(addToUploadDB).not.toHaveBeenCalled();
+            expect(res.status).toHaveBeenCalledWith(500);
+            expect(res.send).toHaveBeenCalledWith('Error processing file upload: error')
+        });
+    })
+
+    describe('getUpload', () => {
+        it('sends a 400 if no id is present', async () => {
+            let req = {
+                params: {}
+            };
+            let res = mockRes();
+
+            res.status.mockImplementation(() => res);
+
+            await getUpload(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.send).toHaveBeenCalledWith('Must provide an id')
+        });
+
+        it('sends a 400 if the upload is still being processed', async () => {
+            let req = {
+                params: {
+                    id: 'testId'
+                }
+            };
+            let res = mockRes();
+
+            res.status.mockImplementation(() => res);
+            getProgressById.mockImplementation(() => 50);
+
+            await getUpload(req, res);
+
+            expect(getProgressById).toHaveBeenCalledWith('testId');
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.send).toHaveBeenCalledWith("Upload still being processed. Visit '/status/{uploadId}")
+        });
+
+        it('sends a 200 and upload processed data if processing complete', async () => {
+            let req = {
+                params: {
+                    id: 'testId'
+                }
+            };
+            let res = mockRes();
+            const uploadData = {
+                results: [{value: 'test'}],
+                processedRecords: 1,
+                failedRecords: 0,
+                details: []
+            };
+
+            res.status.mockImplementation(() => res);
+            res.setHeader.mockImplementation(() => res);
+            getProgressById.mockImplementation(() => 100);
+            getUploadDataById.mockImplementation(() => uploadData)
+
+            await getUpload(req, res);
+
+            expect(getProgressById).toHaveBeenCalledWith('testId');
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.send).toHaveBeenCalledWith({
+                totalRecords: 1,
+                processedRecords: 1,
+                failedRecords: 0,
+                details: []
+            })
+        });
     })
 })
